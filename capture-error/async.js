2a0978bbbe35ea9a9eb1d85282c70854c0edac9a
Gareth Mc Cumskey <gareth.mccumskey@serverless.com>
2020-02-05 17:29:22 +0200

    added async function

---
+++ capture-error/async.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports.capture = async (event, context) => {
+  //Do what I need to here
+
+  try {
+    throw new Error('An error .. ermagherd')
+  } catch(thrownError) {
+    console.log('An error occurred')
+    console.log(thrownError)
+    if (context.hasOwnProperty('serverlessSdk')) {
+      context.serverlessSdk.captureError(thrownError)
+    }
+  }
+
+
+  return true
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
